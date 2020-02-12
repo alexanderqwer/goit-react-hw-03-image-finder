37db397e99f3ef752a242d7f74e6583ec6c3c410
Alexanderqwer <llkkfi27456@gmail.com>
2020-02-12 18:02:43 +0200

    Updates

---
+++ precache-manifest.5304d7ba6aa07d9389bd54d605c9a499.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "032c02257ac76f8fe1d1513cd67b8348",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "85012b9ecbe009e14188",
+    "url": "/goit-react-hw-03-image-finder/static/css/2.32130bdc.chunk.css"
+  },
+  {
+    "revision": "98812e8cefa40fae2933",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.a73ebd83.chunk.css"
+  },
+  {
+    "revision": "85012b9ecbe009e14188",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.4f432630.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.4f432630.chunk.js.LICENSE"
+  },
+  {
+    "revision": "98812e8cefa40fae2933",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.e011267e.chunk.js"
+  },
+  {
+    "revision": "c62a3b045597217d18ed",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.b05354c0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-03-image-finder/precache-manifest.6772b01c44a91d9123ccce25af0eadbb.js"
+  "/goit-react-hw-03-image-finder/precache-manifest.5304d7ba6aa07d9389bd54d605c9a499.js"
 );
 
 self.addEventListener('message', (event) => {
